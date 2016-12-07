3b2bd708554ba7645be632c20a56ca5787393436
Leonardo Minora <leonardo.minora@gmail.com>
2016-12-07 10:18:24 -0300

    Ajuste no console.log para exibir mensagens durante a comunicação
    Changes to be committed:
    	modified:   lib/mirobot.js
    	modified:   server.js

--- server.js
+++ server.js
@@ -1,19 +1,24 @@
 #!/usr/bin/env node
 
 var Server  = require('ws').Server,
-    s       = new Server({port: 8899}),
-    Mirobot = require('./lib/mirobot.js').Mirobot,
-    m       = new Mirobot();
+s       = new Server({port: 8899}),
+Mirobot = require('./lib/mirobot.js').Mirobot,
+m       = new Mirobot();
+
+console.log("Server stated!");
 
 s.on('connection', function(ws) {
-  console.log("New Connection");
-  
-  var respond = function(msg){
-    ws.send(JSON.stringify(msg));
-  }
-  
-  ws.on('message', function(data, flags) {
-    msg = JSON.parse(data)
-    m.handle_msg(msg, respond);
-  });
+    console.log("# New Connection");
+
+    var respond = function(msg){
+        message = JSON.stringify(msg);
+        console.log('=> Send message: %s', message);
+        ws.send(message);
+    }
+
+    ws.on('message', function(data, flags) {
+        msg = JSON.parse(data)
+        console.log("=> Received message: %s", JSON.stringify(msg));
+        m.handle_msg(msg, respond);
+    });
 });
